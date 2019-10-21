94860214f788b923384abc96ed8d0b9cf0006439
Ramon Sanchez <songwright66@gmail.com>
2019-10-20 17:04:23 -0700

    Updated apiRoutes with post delete functions

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,81 +1,49 @@
 // LOAD DATA
 // Link routes to journal data.
 var journalData = require("../data/journalData");
-
+console.log("first time:", journalData)
 // ROUTING
-module.exports = function(app) {
+module.exports = function (app) {
 
   // API GET request
-  app.get("/api/journals", function(req, res) {
+  app.get("/api/journals", function (req, res) {
+    console.log("GET: ", journalData)
     res.json(journalData);
   });
 
   // API POST request
-  app.post("/api/journals", function(req, res) {
+  app.post("/api/journals", function (req, res) {
+    console.log("POST API")
     journalData.push(req.body);
+    console.log("after post:", journalData)
+    // re writte the file  fs.write
+    res.json("saved")
   });
+
+  app.delete("/api/journals/:index", function (req, res) {
+    console.log("DELETE API")
+    var elem = parseInt(req.params.index)
+    console.log("elem:", elem)
+    var tempjournal = []
+    for (var i = 0; i < journalData.length; i++) {
+      console.log(i, elem)
+      if (i !== elem) {
+        tempjournal.push(journalData[i])
+      }
+    }
+    console.log(tempjournal)
+    journalData = tempjournal
+
+    //
+    //  let newarray = journalData(note => {return note.textTitle !== req.body})
+    //   journalData = newarry
+    // elimate the elemn with the index in the array
+    console.log("after delete:", journalData)
+    res.json("delete done")
+    // re writte the file  fs.write
+  });
+
+
 }
 
 
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
-
-// var tableData = require("../data/tableData");
-
-
-// ===============================================================================
-// ROUTING
-// ===============================================================================
-
-// module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
-
-//   app.get("/api/tables", function(req, res) {
-//     res.json(tableData);
-//   });
-
-//   app.get("/api/waitlist", function(req, res) {
-//     res.json(waitListData);
-//   });
-// }
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
-
-  // app.post("/api/tables", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
-  //   if (tableData.length < 5) {
-  //     tableData.push(req.body);
-  //     res.json(true);
-  //   }
-  //   else {
-  //     waitListData.push(req.body);
-  //     res.json(false);
-  //   }
-  // });
-
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
-  // app.post("/api/clear", function(req, res) {
-    // Empty out the arrays of data
-//     tableData.length = 0;
-//     waitListData.length = 0;
-
-//     res.json({ ok: true });
-//   });
-// };
